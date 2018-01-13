3bb46a24f71d315175000433afce36d18473c4c2
Maxime BLANCHARD <maxime.blanchard@epitech.eu>
2018-01-13 15:35:01 +0100

    Version 1.0.0

---
+++ spec/epitech-utils-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EpitechUtilsView from '../lib/epitech-utils-view';
+
+describe('EpitechUtilsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
